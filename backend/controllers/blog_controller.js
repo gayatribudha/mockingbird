0983cbede6d8179e64835c53e2157743dca0d33c
gayatribudha <gayatribudha5@gmail.com>
2021-07-02 14:53:48 +0545

    Add error handlers to the controllers.

--- backend/controllers/blog_controller.js
+++ backend/controllers/blog_controller.js
@@ -1,51 +1,103 @@
 //importing blog model
 const Blog = require('../models/blog_model');
 
-// list all stored blogs
+// list all blogs
 exports.list_blogs = function (req, res) {
-    Blog.find(function (err, blog) {
-        if (err) return next(err);
-        res.send(blog);
-    })
+
+    Blog.find().select('-__v').then(blogs => {
+        res.status(200).json(blogs);
+    }).catch(error => {
+        // log on console
+        console.log(error);
+
+        res.status(500).json({
+            message: "Error!",
+            error: error
+        });
+    });
 };
 
 //create blog
-exports.createBlog = function(req, res){
+exports.createBlog = function (req, res) {
     const blog = new Blog({
         author: req.body.author,
         title: req.body.title,
         description: req.body.description
     });
-    blog.save(function (err) {
-        if (err) {
-            return next(err);
-        }
-        res.send('Blog Created successfully.')
-    })
-}
 
-//give specified blog detail
-exports.blog_detail = function (req, res) {
-    Blog.findById(req.params.id, function (err, blog) {
-        if (err) return next(err);
-        res.send(blog);
-    })
+    blog.save().then(data => {
+        res.status(200).json(data);
+    }).catch(err => {
+        res.status(500).json({
+            message: "Fail!",
+            error: err.message
+        });
+    });
 };
 
-//update specified blog detail
+//give single blog
+exports.blog_detail = function (req, res) {
+
+    Blog.findById(req.params.id).select('-__v')
+        .then(blog => {
+            res.status(200).json(blog);
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: "Blog not found with id " + req.params.id,
+                    error: err
+                });
+            }
+            return res.status(500).send({
+                message: "Error retrieving Blog with id " + req.params.id,
+                error: err
+            });
+        });
+};
+
+//find blog and update it
 exports.blog_update = function (req, res) {
-    Blog.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, blog) {
-        if (err) return next(err);
-        res.send('Blog udpated.');
-    });
+    
+    Blog.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        {new: true}
+    ).select('-__v')
+        .then(blog => {
+            if (!blog) {
+                return res.status(404).send({
+                    message: "Error -> Cannot update a blog with id = " + req.params.id,
+                    error: "Not Found!"
+                });
+            }
+
+            res.status(200).json(blog);
+        }).catch(err => {
+            return res.status(500).send({
+                message: "Error -> Cannot update a blog with id = " + req.params.id,
+                error: err.message
+            });
+        });
 };
 
 //delete specified blog
 exports.blog_delete = function (req, res) {
-    Blog.findByIdAndRemove(req.params.id, function (err) {
-        if (err) return next(err);
-        res.send('Deleted successfully!');
-    })
+
+    Blog.findByIdAndRemove(req.params.id).select('-__v -_id')
+        .then(blog => {
+            if (!blog) {
+                res.status(404).json({
+                    message: "Does Not exist a Blog with id = " + req.params.id,
+                    error: "404",
+                });
+            }
+            res.status(200).json({});
+        }).catch(err => {
+            return res.status(500).send({
+                message: "Error -> Cannot delete a blog with id = " + req.params.id,
+                error: err.message
+            });
+        });
 };
 
 
--- backend/controllers/story_controller.js
+++ backend/controllers/story_controller.js
@@ -4,10 +4,17 @@ const Story = require('../models/story_model');
 
 // list all stored stories
 exports.list_stories = function (req, res) {
-    Story.find(function (err, story) {
-        if (err) return next(err);
-        res.send(story);
-    })
+    Story.find().select('-__v').then(stories => {
+        res.status(200).json(stories);
+    }).catch(error => {
+        // log on console
+        console.log(error);
+
+        res.status(500).json({
+            message: "Error!",
+            error: error
+        });
+    });
 };
 
 //create story
@@ -17,36 +24,76 @@ exports.createStory = function(req, res){
         title: req.body.title,
         description: req.body.description
     });
-    story.save(function (err) {
-        if (err) {
-            return next(err);
-        }
-        res.send('Story Created successfully.')
-    })
-}
+    story.save().then(data => {
+        res.status(200).json(data);
+    }).catch(err => {
+        res.status(500).json({
+            message: "Fail!",
+            error: err.message
+        });
+    });
+};
 
 //give specified story detail
 exports.story_detail = function (req, res) {
-    Story.findById(req.params.id, function (err, story) {
-        if (err) return next(err);
-        res.send(story);
-    })
+    Story.findById(req.params.id).select('-__v')
+        .then(story => {
+            res.status(200).json(story);
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: "Story not found with id " + req.params.id,
+                    error: err
+                });
+            }
+            return res.status(500).send({
+                message: "Error retrieving Story with id " + req.params.id,
+                error: err
+            });
+        });
 };
 
 //update specified story detail
 exports.story_update = function (req, res) {
-    Story.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, story) {
-        if (err) return next(err);
-        res.send('Story udpated.');
-    });
+    Story.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        {new: true}
+    ).select('-__v')
+        .then(story => {
+            if (!story) {
+                return res.status(404).send({
+                    message: "Error -> Cannot update a story with id = " + req.params.id,
+                    error: "Not Found!"
+                });
+            }
+
+            res.status(200).json(story);
+        }).catch(err => {
+            return res.status(500).send({
+                message: "Error -> Cannot update a story with id = " + req.params.id,
+                error: err.message
+            });
+        });
 };
 
 //delete specified story
 exports.story_delete = function (req, res) {
-    Story.findByIdAndRemove(req.params.id, function (err) {
-        if (err) return next(err);
-        res.send('Deleted successfully!');
-    })
+    Story.findByIdAndRemove(req.params.id).select('-__v -_id')
+        .then(story => {
+            if (!story) {
+                res.status(404).json({
+                    message: "Does Not exist a story with id = " + req.params.id,
+                    error: "404",
+                });
+            }
+            res.status(200).json({});
+        }).catch(err => {
+            return res.status(500).send({
+                message: "Error -> Cannot delete a story with id = " + req.params.id,
+                error: err.message
+            });
+        });
 };
 
 
