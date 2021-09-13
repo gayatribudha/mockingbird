dbc0913235df16c3685a9a292d09eef70c676a36
gayatribudha <gayatribudha5@gmail.com>
2021-09-13 23:31:12 +0545

    Dynamic check box for each blog.

--- src/components/ToggleBtn/ToggleBtn.js
+++ src/components/ToggleBtn/ToggleBtn.js
@@ -3,24 +3,26 @@ import React, { useState, useEffect } from 'react';
 
 import './ToggleBtn.css';
 
+export default function ToggleBtn({ blogId }) {
 
-
-export default function ToggleBtn() {
     const [isClicked, setIsClicked] = useState(false);
+
     const handleCheckbox = () => {
         if (isClicked == true) {
             setIsClicked(false);
         } else {
             setIsClicked(true);
+
         }
     }
+    console.log(isClicked);
     return (
         <form className="float-right">
             <div id="toggle-btn" className="d-flex">
                 <p className="mr-2">Publish</p>
-                <input hidden type="checkbox" checked={isClicked} id="checkbox-input"
-                    onChange={handleCheckbox} />
-                <label for="checkbox-input" className="round-slider-container">
+                <input hidden type="checkbox" checked={isClicked} id={blogId} className="checkbox-input" onChange={handleCheckbox} />
+
+                <label for={blogId} className="round-slider-container">
                     <div></div>
                     <div></div>
                     <div className="round-slider"></div>
