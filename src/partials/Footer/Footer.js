a09e2f975017ae91e6e30e54070eeeca48c2d52c
gayatribudha <gayatribudha5@gmail.com>
2021-10-02 23:09:31 +0545

    Integrate EmailJS to send form data to gmail directly.

--- src/partials/Footer/Footer.js
+++ src/partials/Footer/Footer.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
+import swal from 'sweetalert';
+
 import './Footer.css';
 import logo from '../../assets/images/logo.png';
 import telephone from '../../assets/images/telephone.png';
@@ -13,6 +16,26 @@ import tweeter from '../../assets/images/tweeter.png';
 
 
 export default function Footer() {
+
+    const form = useRef();
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('service_obj57eu', 'template_t0vkr4w', form.current, 'user_tQgKJeEFrEQVUVbbZELAz')
+            .then((result) => {
+                // console.log(result.text);
+                swal({
+                    title: "Sent!!",
+                    text: "Thank you for reaching to us.",
+                    icon: "success"
+                });
+                e.target.reset();
+            }, (error) => {
+                console.log(error.text);
+            });
+    }
+
     return (
         <div className="container-fluid mr-md-0 mr-lg-0 pr-md-0 pr-lg-0" id="footer">
             <div className="ml-md-5 ml-lg-5 pl-md-2 pl-lg-2">
@@ -67,7 +90,7 @@ export default function Footer() {
                     </div>
                 </div >
                 <div className="col-sm-12 col-md-8 col-lg-8 mr-md-0 mr-lg-0 pr-sm-0 pr-md-0 pr-lg-0">
-                    <form className="query-form">
+                    <form className="query-form" ref={form} onSubmit={sendEmail}>
                         <div className="mx-md-5 mx-lg-5 px-md-5 px-lg-5 py-md-5 py-lg-5 py-2">
                             <div className="mx-3 mr-md-5 mx-lg-5 px-md-3 px-lg-3">
                                 <h3 className="form-title">We are open to your queries.</h3>
@@ -75,9 +98,9 @@ export default function Footer() {
                             information remains confidential.</p>
                             </div>
                             <div className="mx-3 mr-md-5 mx-lg-5 px-md-3 px-lg-3">
-                                <input type="text" className="form-input" placeholder="Name" /><br />
-                                <input type="email" className="form-input mt-4" placeholder="Email" /> <br />
-                                <textarea className="text-area mt-4" placeholder="Query" /> <br />
+                                <input required type="text" className="form-input" placeholder="Name" name="name" /><br />
+                                <input required type="email" className="form-input mt-4" placeholder="Email" name="email" /> <br />
+                                <textarea required className="text-area form-input mt-4" placeholder="Query" name="query" /> <br />
                                 <button className="form-button mt-4" type="submit">SEND</button>
                             </div>
                         </div>
